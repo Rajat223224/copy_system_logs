264c3f9939b7ae3d6c4d5eb650b84e9965b3cd61
Rajat Jaiswal <125128631+Rajat223224@users.noreply.github.com>
2023-02-22 20:39:42 +0530

    Add files via upload
    This is client file.
    1. client.py install in hacker pc client.py receive a victim logs by server.py application.
    =======> Don't misuse this <===========

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+
+# Initialize Socket Instance
+sock = socket.socket()
+print ("Socket created successfully.")
+
+# Defining port and host
+port = 8800
+host = 'localhost'
+
+# Connect socket to the host and port
+sock.connect((host, port))
+print('Connection Established.')
+# Send a greeting to the server
+sock.send('A message from the client'.encode())
+
+# Write File in binary
+file = open('event_log2.txt', 'wb')
+
+# Keep receiving data from the server
+line = sock.recv(1024)
+
+while(line):
+    file.write(line)
+    line = sock.recv(1024)
+
+print('File has been received successfully.')
+
+file.close()
+sock.close()
+print('Connection Closed.')
